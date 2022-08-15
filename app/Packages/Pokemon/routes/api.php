a8cac9b2a2d632899afa87cf9232092214585678
RumusBin <komlikov.r@gmail.com>
2022-08-15 19:03:32 +0100

    Change some names

--- app/Packages/Pokemon/routes/api.php
+++ app/Packages/Pokemon/routes/api.php
@@ -4,7 +4,7 @@ use Pokemon\Http\Controllers\Api\PokemonApiController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['api', 'only-ajax'])
-    ->prefix('acquire/api')
+    ->prefix('pokemons/api')
     ->group(static function () {
         Route::prefix('v1')->group(static function () {
             Route::apiResource('pokemons', PokemonApiController::class);
--- app/Packages/Pokemon/routes/web.php
+++ app/Packages/Pokemon/routes/web.php
@@ -4,7 +4,7 @@ use App\Packages\Acquire\Http\Controllers\PokemonController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('web')
-    ->prefix('acquire')
+    ->prefix('pokemons')
     ->group(static function () {
     Route::get('/', [PokemonController::class, 'index']);
 });
