4acc758dd48152e69866d294b7c064e9ec8ae36a
RumusBin <komlikov.r@gmail.com>
2022-08-20 20:44:54 +0100

    Add abilities

---
+++ resources/js/Components/Abilities.vue
@@ -0,0 +1,61 @@
+<template>
+    <div>
+        <div v-if="showAbility" class="overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none justify-center items-center flex">
+            <div class="relative w-auto my-6 mx-auto max-w-6xl">
+                <!--content-->
+                <div class="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                    <!--header-->
+                    <div class="flex items-start justify-between p-5 rounded-t">
+                        <h3>Abilities</h3>
+                        <button
+                            class="p-1 ml-auto bg-transparent border-0 text-black float-right text-3xl
+                            leading-none font-semibold outline-none focus:outline-none" v-on:click="closeAbility()"
+                        >
+                          <span class=" text-red-300 h-6 w-6 text-2xl block outline-none focus:outline-none">
+                            ×
+                          </span>
+                        </button>
+                    </div>
+                    <!--body-->
+                    <div v-for="ability in abilities" class="relative mt-200 p-6 flex-auto border-b border-solid border-yellow-400">
+                        <p class="my-4 text-blue-500 text-lg leading-relaxed">
+                            Name: {{ability.name}}
+                        </p>
+                        <p class="my-4 text-blue-500 text-lg leading-relaxed">
+                            Url: {{ability.url}}
+                        </p>
+                        <p class="my-4 text-blue-500 text-lg leading-relaxed">
+                            Type: {{ability.is_hidden ? "Hidden" : "Not hidden"}}
+                        </p>
+                    </div>
+                    <!--footer-->
+                    <div class="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+<!--                        <button class="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150" type="button" v-on:click="toggleModal()">-->
+<!--                            Save Changes-->
+<!--                        </button>-->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div v-if="showAbility" class="opacity-25 fixed inset-0 z-40 bg-black"></div>
+    </div>
+</template>
+
+<script>
+import {reactive, toRefs} from "vue";
+
+export default {
+        name: "Abilities",
+        props: ["showAbility", "abilities"],
+        setup (props, {emit}) {
+            const state = reactive({
+                closeAbility
+            });
+            function closeAbility () {
+                emit('closeAbility');
+            }
+
+            return {... toRefs(state)}
+        }
+}
+</script>
--- resources/js/Components/PokemonIndex.vue
+++ resources/js/Components/PokemonIndex.vue
@@ -29,8 +29,12 @@
                         <p class="text-sm text-green-500">Base experience: {{ pokemon.base_experience }}</p>
                         <p class="text-sm text-gray-400">Weight: {{ pokemon.weight }}</p>
                         <p class="text-sm text-gray-400">Height: {{ pokemon.height }}</p>
-                        <p class="text-sm text-pink-700">Abilities: {{ pokemon.abilities.length }}</p>
-
+                        <a
+                            href="#"
+                            @click="triggerAbility(pokemon.abilities)"
+                            class="text-sm text-pink-700"
+                            @closeAbility="triggerAbility"
+                        >Abilities: {{ pokemon.abilities.length }}</a>
                         <div class="mt-4">
                             <a
                                 href="#"
@@ -43,82 +47,7 @@
                     </div>
                 </div>
             </div>
-        </section>
-        <section>
-            <!-- Button trigger modal -->
-            <button type="button" class="px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out" data-bs-toggle="modal" data-bs-target="#exampleModal">
-                Launch demo modal
-            </button>
-            <!-- Modal -->
-            <div class="modal fade fixed top-0 left-0 hidden w-full h-full outline-none overflow-x-hidden overflow-y-auto"
-                 id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div class="modal-dialog relative w-auto pointer-events-none">
-                    <div
-                        class="modal-content border-none shadow-lg relative flex flex-col w-full pointer-events-auto bg-white bg-clip-padding rounded-md outline-none text-current">
-                        <div
-                            class="modal-header flex flex-shrink-0 items-center justify-between p-4 border-b border-gray-200 rounded-t-md">
-                            <h5 class="text-xl font-medium leading-normal text-gray-800" id="exampleModalLabel">Modal title</h5>
-                            <button type="button"
-                                    class="btn-close box-content w-4 h-4 p-1 text-black border-none rounded-none opacity-50 focus:shadow-none focus:outline-none focus:opacity-100 hover:text-black hover:opacity-75 hover:no-underline"
-                                    data-bs-dismiss="modal" aria-label="Close"></button>
-                        </div>
-                        <div class="modal-body relative p-4">
-                            Modal body text goes here.
-                        </div>
-                        <div
-                            class="modal-footer flex flex-shrink-0 flex-wrap items-center justify-end p-4 border-t border-gray-200 rounded-b-md">
-                            <button type="button" class="px-6
-          py-2.5
-          bg-purple-600
-          text-white
-          font-medium
-          text-xs
-          leading-tight
-          uppercase
-          rounded
-          shadow-md
-          hover:bg-purple-700 hover:shadow-lg
-          focus:bg-purple-700 focus:shadow-lg focus:outline-none focus:ring-0
-          active:bg-purple-800 active:shadow-lg
-          transition
-          duration-150
-          ease-in-out" data-bs-dismiss="modal">Close</button>
-                            <button type="button" class="px-6
-      py-2.5
-      bg-blue-600
-      text-white
-      font-medium
-      text-xs
-      leading-tight
-      uppercase
-      rounded
-      shadow-md
-      hover:bg-blue-700 hover:shadow-lg
-      focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0
-      active:bg-blue-800 active:shadow-lg
-      transition
-      duration-150
-      ease-in-out
-      ml-1">Save changes</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <abilities :show-ability="showAbility" :abilities="abilitiesShowSet" @closeAbility="triggerAbility"></abilities>
         </section>
     </div>
 
@@ -152,19 +81,22 @@
 <script>
 import {reactive, toRefs, computed, onMounted} from "vue";
 import Pagination from "@/Components/Inertia/Pagination.vue ";
+import Abilities from "@/Components/Abilities.vue";
 
 export default {
     name: 'Home',
-    components: {Pagination},
+    components: {Pagination, Abilities},
     setup() {
         const state = reactive({
-            pokemonsDataChunks: [],
+            abilitiesShowSet: [],
             totalData: {},
             urlIdLookUp: {},
             text: "",
             pokemonsLength: 0,
             pokemonsData: [],
-            getPokemons
+            showAbility: false,
+            getPokemons,
+            triggerAbility
             // filteredPokemons: computed(() => updatePokemons())
         });
         onMounted(getPokemons);
@@ -190,6 +122,13 @@ export default {
                     //     {}
                     // )
                 });
+        }
+
+        function triggerAbility(abilities) {
+            if (!state.showAbility) {
+                state.abilitiesShowSet = abilities;
+            }
+            state.showAbility = !state.showAbility;
         }
 
         function sliceIntoChunks(setOfElements, chunkSize) {
